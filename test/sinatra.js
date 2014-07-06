94bc0e4dd944a1fdd4c139ff820b15a6d88a0f9d
Viktor Justo <viktor@vjusto.com>
2014-07-06 18:19:00 -0400

    Including test file.

---
+++ test/sinatra.js
@@ -0,0 +1,19 @@
+var should = require("should"),
+    running = require("is-running"),
+    sinatra = require("../tasks/sinatra");
+
+describe("grunt-sinatra", function(){
+  it("should run the server", function(){
+    //code here
+  });
+  it("should kill the server", function(){
+    //code here
+  })
+  it("should restart a running server", function(){
+    //code here
+  })
+  it("should kill the server when the task finishes", function(){
+    //code here
+  })
+})
+running(pid).
